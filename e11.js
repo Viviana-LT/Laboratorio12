433be040dcd7742cd049447773d161253293342c
Viviana-LT <vlinares.@unsa.edu.pe>
2025-10-27 10:25:00 -0500

    commit 2

---
+++ e11.js
@@ -0,0 +1,6 @@
+const esPositivo = (num) =>{
+    if(num>0) return true;
+    else return false;
+}
+let n=31;
+alert("¿Es positivo? "+esPositivo(n))
---
+++ e12.js
@@ -0,0 +1,10 @@
+const componerTransformaciones = (f1,f2) =>{
+    
+}
+const mayusculas = (texto) =>{
+    let textoM=(texto.toUpperCase());
+    return textoM;
+}
+const agregarSigno = (texto) =>{
+    return texto+"!";
+}
